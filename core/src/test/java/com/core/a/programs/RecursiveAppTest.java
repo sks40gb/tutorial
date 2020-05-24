63560d3cd3265563b4d8dfad5b88e0e2a17222f8
sunilsingh <sks.256@gmail.com>
2020-05-24 22:01:01 +0530

    maven junit

--- core/src/test/java/com/core/a/programs/RecursiveAppTest.java
+++ core/src/test/java/com/core/a/programs/RecursiveAppTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RecursiveApp {
+public class RecursiveAppTest {
 
     @Test
     public void perform(){
