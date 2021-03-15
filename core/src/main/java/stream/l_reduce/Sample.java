59652aaa79552de59d243ea8941187801274db1a
Sunil <sks.256@gmail.com>
2021-03-16 00:11:52 +0530

    Added junit 5

---
+++ core/src/main/java/stream/l_reduce/Sample.java
@@ -0,0 +1,26 @@
+package stream.l_reduce;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        concate();
+        factorial();
+    }
+
+
+    public static void concate(){
+        List<String> items = List.of("A","B","C","D");
+        String output = items.stream().reduce("",(result, item) -> result +"|" + item);
+        System.out.println(output);
+    }
+
+    public static void factorial() {
+        int factorialOf = 5;
+        int result = IntStream.rangeClosed(1, factorialOf).reduce(1, (product, n) -> product * n);
+        System.out.format("Factorial of %s is %s", factorialOf, result);
+    }
+
+}
---
+++ core/src/test/java/ApplicationTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testMethod(){
+        System.out.println("hello test");
+    }
+}
