ff42fc03e5d0f622656527cafd6e7ce48442784f
Sunil <sks.256@gmail.com>
2021-03-16 19:11:33 +0530

    Stream flatMap

--- core/src/test/java/StreamCreationTest.java
+++ core/src/test/java/StreamCreationTest.java
@@ -2,7 +2,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,6 +32,13 @@ public class StreamCreationTest {
 
     }
 
+    private Employee findById(Integer id){
+        Employee employee = empList.stream().filter(e -> e.getId() == id).findFirst().orElse(null);
+        when(employeeRepository.findById(id)).thenReturn(employee);
+        return employeeRepository.findById(id);
+    }
+
+    // of
     @Test
     public void stream_creation_from_of() {
 
@@ -46,6 +55,7 @@ public class StreamCreationTest {
 
     }
 
+    // forEach
     @Test
     public void whenIncrementSalaryForEachEmployee_thenApplyNewSalary() {
 
@@ -61,14 +71,52 @@ public class StreamCreationTest {
     @Test
     public void whenIdMapToEmployee_thenGetMatchedEmployees(){
         Integer ids[] = {1,3};
-        List<Employee> employeeList = Stream.of(ids).map(id -> employeeRepository.findById(id))
+        List<Employee> employeeList = Stream.of(ids).map(id -> findById(id))
             .collect(Collectors.toList());
         assertEquals(2, employeeList.size());
     }
 
-    private EmployeeRepository findById(Employee employee){
-        when(employeeRepository.findById(employee.getId())).thenReturn(employee);
-        return employeeRepository;
+    // collect
+    @Test
+    public void whenCollectStreamToList_thenGetList() {
+        List<Employee> employees = empList.stream().collect(Collectors.toList());
+
+        assertEquals(empList, employees);
+    }
+
+    // filter
+    @Test
+    public void whenFilterEmployees_thenGetFilteredStream() {
+        Integer[] empIds = { 1, 2, 3};
+
+        List<Employee> employees = Stream.of(empIds)
+            .map(this::findById)
+            .filter(Objects::nonNull)
+            .filter(e -> e.getSalary() > 200000)
+            .collect(Collectors.toList());
+
+        assertEquals(Arrays.asList(empList.get(2)), employees);
+    }
+
+    // convert to an Array
+    @Test
+    public void whenStreamToArray_thenGetArray() {
+        Employee[] employees = empList.stream().toArray(Employee[]::new);
+        assertThat(empList.toArray(), equalTo(employees));
+    }
+
+    // flatMap
+    @Test
+    public void whenFlatMapEmployeeNames_thenGetNameStream() {
+        List<List<String>> namesNested = Arrays.asList(
+            Arrays.asList("Jeff", "Bezos"),
+            Arrays.asList("Bill", "Gates"),
+            Arrays.asList("Mark", "Zuckerberg"));
+
+        List<String> namesFlatStream =  namesNested.stream()
+             .flatMap(Collection::stream)
+             .collect(Collectors.toList());
+        assertEquals(namesFlatStream.size(), namesNested.size() * 2);
     }
 
 }
