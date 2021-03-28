efab55917a77b025cfc65d8defaeb6f5bf698d6a
sunilsingh <sks.256@gmail.com>
2021-03-28 11:48:51 +0530

    code refactor

--- core/src/test/java/GenericTest.java
+++ core/src/test/java/GenericTest.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import org.junit.Test;
 
 public class GenericTest {
@@ -8,30 +5,9 @@ public class GenericTest {
     @Test
     public void generic(){
 
-        Cat cat1 = new Cat();
-
-        Factor<Cat> factory = new Factor<>();
-
-        factory.setAnimal(cat1);
-
-        Animal cat2 = factory.getAnimal();
-
-        cat2.makeSound();
-
-        List<Integer> intList = new ArrayList<>();
-        List<Number> numberList = new ArrayList<>();
-
-
-        addNumbers(intList);
-        addNumbers(numberList);
-
-    }
-
-    public void addNumbers(List<? super Integer> list) {
-        for (int i = 1;  i <= 10; i++) {
-//           Number p = list.get(0);
-           list.add(i);
-        }
+        AnimalFactory<? extends Animal> factory = new AnimalFactory<>();
+        Animal cat = factory.getAnimal();
+//        factory.setAnimal(new Cat());
     }
 
     interface Animal{
@@ -44,7 +20,7 @@ public class GenericTest {
         }
     }
 
-    static class Factor<T>{
+    static class AnimalFactory<T>{
 
         private T animal;
 
