1334e5376d1f043afd94d172d6fe8ae66c674928
Sunil <sks.256@gmail.com>
2021-06-06 23:09:26 +0530

    code cleanup

--- core/src/main/java/stream/f_partition/Sample.java
+++ core/src/main/java/stream/f_partition/Sample.java
@@ -3,11 +3,11 @@ package stream.f_partition;
 import java.util.*;
 import static java.util.stream.Collectors.*;
 
-public class Sample {  
-  public static void main(String[] args) {
-    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+public class Sample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-    Object o =  numbers.stream().collect(partitioningBy(x->x%2 == 0));
-    System.out.println(o);
-  }
+        Map<Boolean, List<Integer>> result = numbers.stream().collect(partitioningBy(x -> x % 2 == 0));
+        System.out.println(result);
+    }
 }
