3d116ac35d621f77c0bcc778f07fbece9a3ad4f4
Sunil <sks.256@gmail.com>
2021-06-06 23:22:27 +0530

    code cleanup

--- core/src/main/java/stream/g_joining/Sample.java
+++ core/src/main/java/stream/g_joining/Sample.java
@@ -1,29 +1,30 @@
 package stream.g_joining;
 
 import java.util.*;
+import java.util.stream.Collectors;
 import static java.util.stream.Collectors.*;
 
-public class Sample {  
-  public static List<Person> createPeople() {
-    return Arrays.asList(
-	    new Person("Sara", 20),
-	    new Person("Sara", 22),
-	    new Person("Bob", 20),
-	    new Person("Paula", 32),
-	    new Person("Paul", 32),
-	    new Person("Jack", 2),
-	    new Person("Jack", 72),
-	    new Person("Jill", 12)
-	  );
-  }
+public class Sample {
+    public static List<Person> createPeople() {
+        return Arrays.asList(
+            new Person("Sara", 20),
+            new Person("Sara", 22),
+            new Person("Bob", 20),
+            new Person("Paula", 32),
+            new Person("Paul", 32),
+            new Person("Jack", 2),
+            new Person("Jack", 72),
+            new Person("Jill", 12)
+        );
+    }
 
-  public static void main(String[] args) {
-    List<Person> people = createPeople();
-    
-    System.out.println(
-      people.stream()
-        .map(Person::getName)
-        .map(String::toUpperCase)
-        .collect(joining(", ")));
-  }
+    public static void main(String[] args) {
+        List<Person> people = createPeople();
+        String txt = people.stream()
+            .map(Person::getName)
+            .map(String::toUpperCase)
+            .collect(joining(", "));
+
+        System.out.println(txt);
+    }
 }
--- core/src/main/java/stream/h_grouping/Sample.java
+++ core/src/main/java/stream/h_grouping/Sample.java
@@ -3,27 +3,36 @@ package stream.h_grouping;
 import java.util.*;
 import static java.util.stream.Collectors.*;
 
-public class Sample {  
-  public static List<Person> createPeople() {
-    return Arrays.asList(
-	    new Person("Sara", 20),
-	    new Person("Sara", 22),
-	    new Person("Bob", 20),
-	    new Person("Paula", 32),
-	    new Person("Paul", 32),
-	    new Person("Jack", 2),
-	    new Person("Jack", 72),
-	    new Person("Jill", 12)
-	  );
-  }
+public class Sample {
+    public static List<Person> createPeople() {
+        return Arrays.asList(
+            new Person("Sara", 20),
+            new Person("Sara", 22),
+            new Person("Bob", 20),
+            new Person("Paula", 32),
+            new Person("Paul", 32),
+            new Person("Jack", 2),
+            new Person("Jack", 72),
+            new Person("Jill", 12)
+        );
+    }
 
-  public static void main(String[] args) {
-    List<Person> people = createPeople();
-    
-    Map<String, List<Person>> byName =
-      people.stream()
-        .collect(groupingBy(person -> person.getName()));
-        
-    System.out.println(byName);
-  }
+    public static void main(String[] args) {
+        example1();
+        example2();
+    }
+
+    private static void example1() {
+        List<Person> people = createPeople();
+        Map<String, List<Person>> byName = people.stream()
+            .collect(groupingBy(Person::getName));
+        System.out.println(byName);
+    }
+
+    private static void example2() {
+        List<Person> people = createPeople();
+        Map<String, List<Integer>> nameAndAge = people.stream()
+            .collect(groupingBy(Person::getName, mapping(Person::getAge, toList())));
+        System.out.println(nameAndAge);
+    }
 }
