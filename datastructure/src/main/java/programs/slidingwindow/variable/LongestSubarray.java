19e16bc58e52812808499df5ceb7eca6260a2bc2
Sunil <sks.256@gmail.com>
2021-11-28 19:41:53 +0530

    LongestSubstringWihUniqueCharacters

---
+++ datastructure/src/main/java/programs/slidingwindow/variable/LongestSubarray.java
@@ -0,0 +1,52 @@
+package programs.slidingwindow.variable;
+
+/**
+ * Longest sub-array having sum k
+ *
+ * Note : This will work only with positive numbers
+ *
+ * Input : arr[] = { 10, 5, 2, 7, 1, 9 },  k = 15
+ * Output : 4
+ * The sub-array is {5, 2, 7, 1}.
+ *
+ * Input : arr[] = {-5, 8, -14, 2, 4, 12}, k = -5
+ * Output : 5
+ */
+public class LongestSubarray {
+    public static void main(String[] args) {
+       testWithPositiveNumbersOnly();
+       testWithNegativeNumbers();
+    }
+
+    private static void testWithPositiveNumbersOnly(){
+        int[] arr = {10, 5, 2, 7, 1, 9};
+        int k = 15;
+        int longest = findLongest(arr, k);
+        System.out.println(longest);
+    }
+
+    private static void testWithNegativeNumbers(){
+        int[] arr = {-5, 8, -14, 2, 4, 12};
+        int k = -5;
+        int longest = findLongest(arr, k);
+        System.out.println(longest);
+    }
+
+    private static int findLongest(int[] arr, int k) {
+        int sum = 0;
+        int maxSize = 0;
+        int leftIndex = 0;
+        for (int rightIndex = 0; rightIndex < arr.length ; rightIndex++) {
+            sum += arr[rightIndex];
+            if(sum == k){
+                maxSize = Math.max(maxSize, rightIndex - leftIndex + 1);
+            }else if(sum > k){
+                while(sum > k && leftIndex <= rightIndex){
+                    sum -= arr[leftIndex];
+                    leftIndex++;
+                }
+            }
+        }
+        return maxSize;
+    }
+}
---
+++ datastructure/src/main/java/programs/slidingwindow/variable/LongestSubstringWihUniqueCharacters.java
@@ -0,0 +1,68 @@
+package programs.slidingwindow.variable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Find the longest substring with k unique characters in a given string
+ * Examples :
+ *
+ * "aabbcc", k = 1
+ * Max substring can be any one from {"aa" , "bb" , "cc"}.
+ *
+ * "aabbcc", k = 2
+ * Max substring can be any one from {"aabb" , "bbcc"}.
+ *
+ * "aabbcc", k = 3
+ * There are substrings with exactly 3 unique characters
+ * {"aabbcc" , "abbcc" , "aabbc" , "abbc" }
+ * Max is "aabbcc" with length 6.
+ * "aaabbb", k = 3
+ * There are only two unique characters, thus show error message.
+ */
+public class LongestSubstringWihUniqueCharacters {
+
+    public static void main(String[] args) {
+        String input = "aabbcc";
+        int k = 2;
+        System.out.println(findLongestString(input, k));
+    }
+
+    private static int findLongestString(String input, int k) {
+        int leftIndex = 0;
+        int maxLength = 0;
+        Map<Character, Integer> countMap = new HashMap<>();
+        for (int rightIndex = 0; rightIndex < input.length(); rightIndex++) {
+            addToMap(countMap, input.charAt(rightIndex));
+            if (countMap.size() == k) {
+                maxLength = Math.max(maxLength, rightIndex - leftIndex + 1);
+            } else if (countMap.size() > k) {
+                while (countMap.size() > k) {
+                    removeFromMap(countMap, input.charAt(leftIndex));
+                    leftIndex++;
+                }
+            }
+        }
+        return maxLength;
+    }
+
+    public static void addToMap(Map<Character, Integer> map, char c) {
+        if (map.containsKey(c)) {
+            map.put(c, map.get(c) + 1);
+        } else {
+            map.put(c, 1);
+        }
+    }
+
+    public static void removeFromMap(Map<Character, Integer> map, char c) {
+        if (map.containsKey(c)) {
+            if (map.get(c) == 1) {
+                map.remove(c);
+            } else {
+                map.put(c, map.get(c) - 1);
+            }
+        } else {
+            throw new RuntimeException("Map doesn't contain the key '" + c + "'");
+        }
+    }
+}
