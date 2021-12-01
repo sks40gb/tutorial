1ca7945b3589728f9ae91b1eebe017d0a11d6ac6
Sunil <sks.256@gmail.com>
2021-12-01 16:16:51 +0530

    Recursive

---
+++ datastructure/src/main/java/programs/recursive/easy/Power.java
@@ -0,0 +1,18 @@
+package programs.recursive.easy;
+
+public class Power {
+
+    public static void main(String[] args) {
+        int power = power(2,5);
+        System.out.println(power);
+        System.out.println(Math.pow(2,5));
+    }
+
+    private static int power(int number, int times){
+        if(times == 0){
+            return 1;
+        }
+        return number * power(number, --times);
+    }
+}
+
