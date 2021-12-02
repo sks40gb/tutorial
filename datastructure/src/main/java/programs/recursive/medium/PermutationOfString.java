2b711c6afb3e2c387c61fab71c24e5d46849b8d5
Sunil <sks.256@gmail.com>
2021-12-02 20:06:09 +0530

    Recursive

---
+++ datastructure/src/main/java/programs/recursive/medium/PermutationOfString.java
@@ -0,0 +1,25 @@
+package programs.recursive.medium;
+
+public class PermutationOfString {
+
+    public static void main(String[] args) {
+        String input = "abcd";
+        new PermutationOfString().permutation(input, "");
+    }
+
+    private void permutation(String input, String permutation) {
+
+        if (input.length() == 0) {
+            System.out.println(permutation);
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            String exclusiveString = getExclusiveString(input, i);
+            permutation(exclusiveString, permutation + input.charAt(i));
+        }
+    }
+
+    private String getExclusiveString(String input, int index) {
+        return input.substring(0, index) + input.substring(index + 1);
+    }
+}
