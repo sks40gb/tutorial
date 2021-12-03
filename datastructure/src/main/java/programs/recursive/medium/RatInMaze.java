12a49b2908088578d6fbc456520887f75dbba40c
Sunil <sks.256@gmail.com>
2021-12-03 13:15:47 +0530

    Recursive

--- datastructure/src/main/java/programs/recursive/medium/RatInMaze.java
+++ datastructure/src/main/java/programs/recursive/medium/RatInMaze.java
@@ -1,95 +1,111 @@
 package programs.recursive.medium;
 
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Vector;
 
+/**
+ * https://www.geeksforgeeks.org/rat-in-a-maze-problem-when-movement-in-all-possible-directions-is-allowed/
+ */
 public class RatInMaze {
+    static Vector<String> possiblePaths = new Vector<>();
+    static String path = "";
+    static final int MAX = 5;
+
 
     public static void main(String[] args) {
-        int[][] maze =
-            {
-                {1, 0, 0, 0},
-                {1, 1, 1, 0},
-                {0, 1, 1, 0},
-                {0, 1, 1, 1}
-            };
-        RatInMaze ratInMaze = new RatInMaze();
-        ratInMaze.walk(maze, 4, 4, 0, 0, null);
+        int m[][] = {{1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1},
+            {1, 1, 1, 0, 1},
+            {0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1}};
+        int n = m.length;
+
+        printPath(m, n);
     }
 
-    public void walk(int[][] maze, int m, int n, int i, int j, List<Point> path) {
-        // System.out.println("[" + m + "," + n + "," + i + "," + j + "]");
-//        System.out.println(isVisited(i, j, path));
-        if (path == null) {
-            path = new ArrayList<>();
-        }
+    static boolean isSafe(int row, int col, int m[][],
+                          int n, boolean visited[][]) {
+        if (row == -1 || row == n || col == -1 ||
+            col == n || visited[row][col] ||
+            m[row][col] == 0)
+            return false;
 
-        if (isOutOfRange(m, n, i, j) || isVisited(i, j, path)) {
+        return true;
+    }
+
+    static void printPathUtil(int row, int col, int m[][],
+                              int n, boolean visited[][]) {
+
+        // This will check the initial point
+        // (i.e. (0, 0)) to start the paths.
+        if (row == -1 || row == n || col == -1 ||
+            col == n || visited[row][col] ||
+            m[row][col] == 0)
+            return;
+
+        // If reach the last cell (n-1, n-1)
+        // then store the path and return
+        if (row == n - 1 && col == n - 1) {
+            possiblePaths.add(path);
             return;
         }
-        if (isDestination(m, n, i, j)) {
-            path.add(new Point(i, j));
-            printPath(path);
-            return;
-        }
-        if (isSafe(maze, m, n, i + 1, j)) {
-            path.add(new Point(i, j));
-            walk(maze, m, n, i + 1, j, path);
-            path.remove(path.size()- 1);
-        }
-        if (isSafe(maze, m, n, i - 1, j)) {
-            path.add(new Point(i, j));
-            walk(maze, m, n, i - 1, j, path);
-            path.remove(path.size()- 1);
-        }
-        if (isSafe(maze, m, n, i, j + 1)) {
-            path.add(new Point(i, j));
-            walk(maze, m, n, i, j + 1, path);
-            path.remove(path.size()- 1);
-        }
-        if (isSafe(maze, m, n, i, j - 1)) {
-            path.add(new Point(i, j));
-            walk(maze, m, n, i, j - 1, path);
-            path.remove(path.size()- 1);
-        }
-    }
 
-    private boolean isDestination(int m, int n, int i, int j) {
-        return i == n - 1 && j == m - 1;
-    }
+        // Mark the cell as visited
+        visited[row][col] = true;
 
-    private boolean isOutOfRange(int m, int n, int i, int j) {
-        return (i >= n || i < 0 || j >= m || j < 0);
-    }
+        // Try for all the 4 directions (down, left,
+        // right, up) in the given order to get the
+        // paths in lexicographical order
 
-    private boolean isSafe(int[][] maze, int m, int n, int i, int j) {
-        return (!isOutOfRange(m, n, i, j) && maze[i][j] == 1);
-    }
-
-    private boolean isVisited(int i, int j, List<Point> path) {
-        for (Point point : path) {
-            if (point.x == i && point.y == j) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private static void printPath(List<Point> list) {
-        System.out.println("---------------");
-        for (Point p : list) {
-            System.out.println("[" + p.x + "," + p.y +"]");
-        }
-    }
-
-    class Point {
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
+        // Check if downward move is valid
+        if (isSafe(row + 1, col, m, n, visited)) {
+            path += 'D';
+            printPathUtil(row + 1, col, m, n,
+                visited);
+            path = path.substring(0, path.length() - 1);
         }
 
-        int x;
-        int y;
+        // Check if the left move is valid
+        if (isSafe(row, col - 1, m, n, visited)) {
+            path += 'L';
+            printPathUtil(row, col - 1, m, n,
+                visited);
+            path = path.substring(0, path.length() - 1);
+        }
+
+        // Check if the right move is valid
+        if (isSafe(row, col + 1, m, n, visited)) {
+            path += 'R';
+            printPathUtil(row, col + 1, m, n,
+                visited);
+            path = path.substring(0, path.length() - 1);
+        }
+
+        // Check if the upper move is valid
+        if (isSafe(row - 1, col, m, n, visited)) {
+            path += 'U';
+            printPathUtil(row - 1, col, m, n,
+                visited);
+            path = path.substring(0, path.length() - 1);
+        }
+
+        // Mark the cell as unvisited for
+        // other possible paths
+        visited[row][col] = false;
     }
+
+    // Function to store and print
+// all the valid paths
+    static void printPath(int m[][], int n) {
+        boolean[][] visited = new boolean[n][MAX];
+
+        // Call the utility function to
+        // find the valid paths
+        printPathUtil(0, 0, m, n, visited);
+
+        // Print all possible paths
+        for (int i = 0; i < possiblePaths.size(); i++)
+            System.out.print(possiblePaths.get(i) + " ");
+    }
+
 }
+
