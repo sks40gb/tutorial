771c7c1c3641485b726d55772ee39d52c4599fd2
Sunil <sks.256@gmail.com>
2021-12-06 20:31:32 +0530

    code refactoring

--- datastructure/src/main/java/programs/linkedlist/Test.java
+++ datastructure/src/main/java/programs/linkedlist/Test.java
@@ -4,27 +4,25 @@ public class Test {
 
     public static void main(String[] args) {
         String input = "ABCDEF";
-        System.out.println(findWays(input,""));
+       findWays(input,"");
 //        System.out.println(removeChar("sunil", 4));
         System.out.println(f(6));
     }
 
-    public static int  findWays(String input, String output) {
+    public static void findWays(String input, String output) {
         if (input.length() == 0) {
             System.out.println("input : " + output);
-            return 1;
+            return;
         }
         char c = input.charAt(0);
         output += c;
         String str = removeChar(input, 0);
-        int count = 0;
-        count = count + findWays(str, output);
+        findWays(str, output);
         for (int i = 0; i < str.length(); i++) {
             c = str.charAt(i);
             String newInput = removeChar(str, i);
-            count = count + findWays(newInput,output+c);
+            findWays(newInput,output+c);
         }
-        return 0;
     }
 
     private static String removeChar(String input, int index) {
