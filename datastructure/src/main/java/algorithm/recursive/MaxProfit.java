fa44146fb9596faf35e8cad7d9790703feadf7ea
Sunil <sks.256@gmail.com>
2021-12-07 10:36:43 +0530

    code refactoring

--- datastructure/src/main/java/algorithm/recursive/MaxProfit.java
+++ datastructure/src/main/java/algorithm/recursive/MaxProfit.java
@@ -1,28 +1,60 @@
 package algorithm.recursive;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MaxProfit {
 
     public static void main(String[] args) {
-        int[] arr = {10, 22, 5, 75, 65, 80};
-        maxProfit(arr, 4, true, 0, 0);
+        int[] arr = {12, 14, 17, 10, 14, 13, 12, 15};
+        Map<Key, Integer> memo = new HashMap<>();
+        System.out.println("Max profit : " + maxProfit(arr, 6, true, 0, 0, memo));
     }
 
-    private static void maxProfit(int[] arr, int k, boolean buy, int index, int profit) {
-       if(k == 0){
+    private static int maxProfit(int[] arr, int k, boolean buy, int index, int profit, Map<Key, Integer> memo) {
+        if (k == 0) {
             System.out.println("Profit: " + profit);
-            return;
+            return profit;
         }
         if (index == arr.length) {
             System.out.println("Profit: " + profit);
-            return;
+            return profit;
         }
+        Key key = new Key(buy, index, profit);
+        if (memo.containsKey(key)) {
+            System.out.println("From memo.............");
+            return memo.get(key);
+        }
+        int maxProfit = 0;
         if (buy) {
-            maxProfit(arr, k - 1, false, index + 1, profit - arr[index]);
+            maxProfit = Math.max(maxProfit, maxProfit(arr, k - 1, false, index + 1, profit - arr[index], memo));
         }
         if (!buy) {
-            //Sell
-            maxProfit(arr, k - 1, true, index + 1, profit + arr[index]);
+            maxProfit = Math.max(maxProfit, maxProfit(arr, k - 1, true, index + 1, profit + arr[index], memo));
         }
-        maxProfit(arr, k, buy, index + 1, profit);
+        maxProfit = Math.max(maxProfit, maxProfit(arr, k, buy, index + 1, profit, memo));
+        memo.put(key, maxProfit);
+        return maxProfit;
+    }
+
+    static class Key {
+        public boolean buy;
+        public int index;
+        public int profit;
+
+        public Key(boolean buy, int index, int profit) {
+            this.buy = buy;
+            this.index = index;
+            this.profit = profit;
+        }
+
+        public boolean equals(Object o) {
+            Key key = (Key) o;
+            return key.buy == buy && key.index == index && key.profit == profit;
+        }
+
+        public int hashCode() {
+            return index;
+        }
     }
 }
