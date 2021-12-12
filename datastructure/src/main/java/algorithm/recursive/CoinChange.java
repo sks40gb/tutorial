a5552da627c20229bd6cd7c3f36e786913d7bc01
Sunil <sks.256@gmail.com>
2021-12-12 19:55:37 +0530

    code refactoring

--- datastructure/src/main/java/algorithm/recursive/CoinChange.java
+++ datastructure/src/main/java/algorithm/recursive/CoinChange.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 /**
  * https://www.geeksforgeeks.org/coin-change-dp-7/
+ *
+ * Given a value N, if we want to make change for N cents, and we have infinite supply of
+ * each of S = { S1, S2, .. , Sm} valued coins, how many ways can we make the change?
+ * The order of coins doesn’t matter.
+ *
+ * For example, for N = 4 and S = {1,2,3},
+ * there are four solutions: {1,1,1,1},{1,1,2},{2,2},{1,3}. So output should be 4.
  */
 public class CoinChange {
 
