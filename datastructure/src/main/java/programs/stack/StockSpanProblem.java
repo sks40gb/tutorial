6b7b8f8c588ba6863a6add7d9852b42836f7b809
Sunil <sks.256@gmail.com>
2021-12-14 23:57:01 +0530

    Data Structure

---
+++ datastructure/src/main/java/programs/stack/StockSpanProblem.java
@@ -0,0 +1,44 @@
+package programs.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * ]
+ * Input   :  {100, 80, 60, 70, 60, 75, 85}
+ * Output  :  {1, 1, 1, 2, 1, 4, 6}
+ */
+public class StockSpanProblem {
+    public static void main(String[] args) {
+        int[] arr = {10, 4, 5, 90, 120, 80};
+        List<Integer> result = resolve(arr);
+        System.out.println(result);
+    }
+
+    private static List<Integer> resolve(int[] arr) {
+        List<Integer> indices = new ArrayList<>();
+        Stack<Integer> indexStack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!indexStack.isEmpty() && arr[indexStack.peek()] < arr[i]) {
+                indexStack.pop();
+            }
+            if (indexStack.isEmpty()) {
+                indices.add(i+1);
+            } else {
+                indices.add(i-indexStack.peek());
+            }
+            indexStack.add(i);
+        }
+        return indices;
+    }
+}
+
+
+
+
+
+
+
+
+
