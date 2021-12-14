cc6016bba9b13aa21ce1ac114b3ec19d55a06e46
Sunil <sks.256@gmail.com>
2021-12-14 21:57:37 +0530

    Data Structure

--- datastructure/src/main/java/programs/stack/NextGreaterElement.java
+++ datastructure/src/main/java/programs/stack/NextGreaterElement.java
@@ -4,32 +4,59 @@ package programs.stack;
 // greater element for all array elements
 // in same order as input.
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Stack;
 
 class NextGreaterElement {
 
     public static void main(String[] args) {
         int[] arr = {11, 13, 21, 3};
-        printNGE(arr);
+        System.out.println(printNGE(arr));
     }
 
-    public static void printNGE(int arr[]) {
-        Stack<Integer> s = new Stack<>();
-        int nge[] = new int[arr.length];
+    public static List<Integer> printNGE(int arr[]) {
+        Stack<Integer> stack = new Stack<>();
+        List<Integer> result = new ArrayList<>();
 
         for (int i = arr.length - 1; i >= 0; i--) {
-            if (!s.empty()) {
-                while (!s.empty()
-                    && s.peek() <= arr[i]) {
-                    s.pop();
+            if (stack.isEmpty()) {
+                result.add(-1);
+                stack.add(arr[i]);
+            } else {
+                while (!stack.isEmpty() && stack.peek() < arr[i]) {
+                    stack.pop();
                 }
+                if (stack.isEmpty()) {
+                    result.add(-1);
+                } else {
+                    result.add(stack.peek());
+                }
+                stack.push(arr[i]);
             }
-            nge[i] = s.empty() ? -1 : s.peek();
-            s.push(arr[i]);
         }
-        for (int i = 0; i < arr.length; i++)
-            System.out.println(arr[i] +
-                " --> " + nge[i]);
+        Collections.reverse(result);
+        return result;
     }
+
+//    public static void printNGE(int arr[]) {
+//        Stack<Integer> s = new Stack<>();
+//        int nge[] = new int[arr.length];
+//
+//        for (int i = arr.length - 1; i >= 0; i--) {
+//            if (!s.empty()) {
+//                while (!s.empty()
+//                    && s.peek() <= arr[i]) {
+//                    s.pop();
+//                }
+//            }
+//            nge[i] = s.empty() ? -1 : s.peek();
+//            s.push(arr[i]);
+//        }
+//        for (int i = 0; i < arr.length; i++)
+//            System.out.println(arr[i] +
+//                " --> " + nge[i]);
+//    }
 
 }
