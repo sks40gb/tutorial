2cd4662c2760f7068a07ffdb943510d48e519cb3
Sunil <sks.256@gmail.com>
2021-12-30 12:37:55 +0530

    Data Structure

--- datastructure/src/main/java/algorithm/twopointer/ContainerWithMostWater.java
+++ datastructure/src/main/java/algorithm/twopointer/ContainerWithMostWater.java
@@ -11,16 +11,12 @@ public class ContainerWithMostWater {
     public int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
-        int i = 0;
-        int j = 0;
         int maxArea = 0;
 
         while (left < right) {
             int area = (right - left) * Math.min(height[left], height[right]);
             if (area > maxArea) {
                 maxArea = area;
-                i = left;
-                j = right;
             }
             if (height[left] < height[right]) {
                 left++;
