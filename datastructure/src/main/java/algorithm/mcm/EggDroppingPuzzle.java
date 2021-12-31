5bc42f381886ba9ed024ac07d184594c9e124cac
Sunil <sks.256@gmail.com>
2021-12-31 21:31:47 +0530

    Data Structure

--- datastructure/src/main/java/algorithm/mcm/EggDroppingPuzzle.java
+++ datastructure/src/main/java/algorithm/mcm/EggDroppingPuzzle.java
@@ -1,10 +1,11 @@
 package algorithm.mcm;
 
+import java.util.Arrays;
+
 /**
  * https://www.geeksforgeeks.org/egg-dropping-puzzle-dp-11/
  *
  * eggDrop(n, k) = 1 + min{max(eggDrop(n – 1, x – 1), eggDrop(n, k – x)), where x is in {1, 2, …, k}}
- *
  */
 public class EggDroppingPuzzle {
 
@@ -25,6 +26,27 @@ public class EggDroppingPuzzle {
             min = Math.min(res, min);
         }
         return min + 1;
+    }
+
+    private static int eggDropDP(int eggs, int floors, int[][] dp) {
+
+        if (dp[eggs][floors] != -1) {
+            System.out.println("from mem");
+            return dp[eggs][floors];
+        }
+
+        if (floors == 0 || floors == 1 || eggs == 1) {
+            return floors;
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (int k = 1; k <= floors; k++) {
+            int res = Math.max(eggDrop(eggs - 1, k - 1), eggDrop(eggs, floors - k));
+            min = Math.min(res, min);
+        }
+        dp[eggs][floors] = min + 1;
+        return dp[eggs][floors];
     }
 
 }
