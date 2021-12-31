76491e84dab687fd82ddcc403147a80d71776dbf
Sunil <sks.256@gmail.com>
2021-12-31 20:58:53 +0530

    Data Structure

--- datastructure/src/main/java/algorithm/mcm/PalindromePartitioning.java
+++ datastructure/src/main/java/algorithm/mcm/PalindromePartitioning.java
@@ -14,7 +14,6 @@ public class PalindromePartitioning {
         String str = "ababbbabbababa";
         System.out.println("Min cuts needed for "
             + "Palindrome Partitioning is " + minPartition(str, 0, str.length() - 1));
-        System.out.println("Min " + minCut(str, 0, str.length()-1));
     }
 
     static boolean isPalindrome(String string, int i, int j) {
@@ -28,8 +27,9 @@ public class PalindromePartitioning {
     }
 
     static int minPartition(String string, int i, int j) {
-        if (i >= j || isPalindrome(string, i, j))
+        if (i >= j || isPalindrome(string, i, j)){
             return 0;
+        }
         int ans = Integer.MAX_VALUE;
         for (int k = i; k < j; k++) {
             int count = minPartition(string, i, k) + minPartition(string, k + 1, j) + 1;
