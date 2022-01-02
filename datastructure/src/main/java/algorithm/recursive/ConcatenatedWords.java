997586295ae01f78ca19f48b1dc5721729448a89
sunilsingh <sks.256@gmail.com>
2022-01-02 19:39:06 +0530

    Data Structure

--- datastructure/src/main/java/algorithm/recursive/ConcatenatedWords.java
+++ datastructure/src/main/java/algorithm/recursive/ConcatenatedWords.java
@@ -12,34 +12,36 @@ import java.util.*;
  *
  * Input: words = ["cat","dog","catdog"]
  * Output: ["catdog"]
- *
  */
 public class ConcatenatedWords {
 
     public static void main(String[] args) {
-        String[] words = {"cat","dog","catdog"};
+        String[] words = {"cat", "dog", "catdog"};
         System.out.println(findAllConcatenatedWordsInADict(words));
     }
+
     public static List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> list = new ArrayList<String>();
-        Set<String> set = new HashSet(Arrays.asList(words));
+        Set<String> set = new HashSet<>(Arrays.asList(words));
 
-        for(String word : words) {
+        for (String word : words) {
             set.remove(word);
-            if(dfs(word, set, "")) list.add(word);
+            if (dfs(word, set, "")) list.add(word);
             set.add(word);
         }
         return list;
     }
 
     private static boolean dfs(String word, Set<String> set, String previous) {
-        if(!previous.equals("")) set.add(previous);
-        if(set.contains(word)) return true;
-
-        for(int i = 1; i < word.length(); i++) {
-            String prefix = word.substring(0,i);
-            if(set.contains(prefix) &&
-                    dfs(word.substring(i,word.length()), set, previous+prefix)) {
+        if (!previous.equals("")) {
+            set.add(previous);
+        }
+        if (set.contains(word)) {
+            return true;
+        }
+        for (int i = 1; i < word.length(); i++) {
+            String prefix = word.substring(0, i);
+            if (set.contains(prefix) && dfs(word.substring(i), set, previous + prefix)) {
                 return true;
             }
         }
