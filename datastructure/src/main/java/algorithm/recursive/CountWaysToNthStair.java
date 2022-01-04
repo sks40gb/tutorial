f9e92abf798d76c7cff86fc72ade89b2c8114708
Sunil <sks.256@gmail.com>
2022-01-04 14:06:25 +0530

    Data Structure

--- datastructure/src/main/java/algorithm/recursive/CountWaysToNthStair.java
+++ datastructure/src/main/java/algorithm/recursive/CountWaysToNthStair.java
@@ -65,8 +65,9 @@ public class CountWaysToNthStair {
         res[1] = 1;
         res[2] = 2;
 
-        for (int i = 3; i <= n; i++)
+        for (int i = 3; i <= n; i++){
             res[i] = res[i - 1] + res[i - 2] + res[i - 3];
+        }
 
         return res[n];
     }
