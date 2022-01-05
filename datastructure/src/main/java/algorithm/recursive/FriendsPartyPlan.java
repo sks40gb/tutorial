aeb8f4cf2df647a797faffecdadfe6dfc62c9156
Sunil <sks.256@gmail.com>
2022-01-05 10:43:13 +0530

    Data Structure

--- datastructure/src/main/java/algorithm/recursive/FriendsPartyPlan.java
+++ datastructure/src/main/java/algorithm/recursive/FriendsPartyPlan.java
@@ -76,12 +76,12 @@ public class FriendsPartyPlan {
     /**
      * Iterative
      */
-    public static int findWays4(int n){
-        int[] res = new int[n+1];
+    public static int findWays4(int n) {
+        int[] res = new int[n + 1];
         res[1] = 1;
         res[2] = 2;
-        for (int i = 3; i <= n ; i++) {
-            res[i] = res[i-1] + (i-1) * res[i-2];
+        for (int i = 3; i <= n; i++) {
+            res[i] = res[i - 1] + (i - 1) * res[i - 2];
         }
         return res[n];
     }
