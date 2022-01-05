c88268ca1eb1ed1e3dea221ebfd23e67d18258c4
Sunil <sks.256@gmail.com>
2022-01-05 19:08:49 +0530

    Data Structure

--- datastructure/src/main/java/algorithm/recursive/latest/HouseRobber.java
+++ datastructure/src/main/java/algorithm/recursive/latest/HouseRobber.java
@@ -20,10 +20,13 @@ package algorithm.recursive.latest;
 public class HouseRobber {
 
     public static void main(String[] args) {
-        int[] input = {2, 7, 9, 3, 1}; // 12
+        int[] input = {2, 7, 9}; // 12
         System.out.println(rob(input, 0));
+        System.out.println(rob2(input, input.length - 1));
+        System.out.println(robDP(input));
     }
 
+    // Recursive method 1
     public static int rob(int[] input, int index) {
         if (index == input.length - 1) {
             return input[index];
@@ -34,6 +37,38 @@ public class HouseRobber {
         int robIt = input[index] + rob(input, index + 2);
         int dontRobIt = rob(input, index + 1);
         return Math.max(robIt, dontRobIt);
+    }
+
+    // Recursive method 2
+    public static int rob2(int[] input, int index) {
+        if (index < 0) {
+            return 0;
+        }
+        if (index == 0) {
+            return input[0];
+        }
+        int robIt = input[index] + rob2(input, index - 2);
+        int skipIt = rob2(input, index - 1);
+        return Math.max(robIt, skipIt);
+    }
+
+    // DP method -> similar to Recursive method 2.
+    // Actually recursive method 2 was created
+    // to build DP solution below.
+    public static int robDP(int[] nums) {
+        int n = nums.length - 1;
+        int[] dp = new int[n + 1];
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i <= n; i++) {
+            int robeIt = nums[i] + dp[i - 2];
+            int skipIt = dp[i - 1];
+            dp[i] = Math.max(robeIt, skipIt);
+        }
+        return dp[n];
     }
 
 }
