ad6aa7d2b2ccb75d727b7135ff0a9f681f4ebf35
Sunil <sks.256@gmail.com>
2022-01-09 12:09:44 +0530

    Data Structure

--- datastructure/src/main/java/algorithm/Test.java
+++ datastructure/src/main/java/algorithm/Test.java
@@ -3,50 +3,91 @@ package algorithm;
 public class Test {
 
     public static void main(String[] args) {
-        int[][] matrix = {
-            {0, 0, 1, 1},
-            {0, 0, 1, 1},
-            {1, 1, 1, 0}
-        };
-        System.out.println(solve(matrix, 0, 0, 1));
+        int[][] board = new int[4][4];
+        System.out.println(solve(board, 4));
+        printBoard(board);
     }
 
-    private static boolean solve(int[][] matrix, int row, int col, int n) {
-
-        if (row < 0
-            || col < 0 //not beyond top
-            || row >= matrix.length // not row to the left
-            || col >= matrix[0].length
-            || matrix[row][col] == -1
-            || (matrix[row][col] == 1 && n == 0)) {
-            return false;
-        }
-
-        if (row == matrix.length - 1 && col == matrix[0].length - 1) {
-            System.out.println("found " + row + "," + col + " v :" + matrix[row][col]);
-            return true;
-
-        }
-
-        System.out.println(row + "," + col + " v: " + matrix[row][col]);
-
-        int preValue = matrix[row][col];
-        if (matrix[row][col] == 1) {
-            n--;
-        }
-
-        matrix[row][col] = -1;
-        System.out.println(" starting from " + row + "," + col);
-
-        int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        for (int d = 0; d < DIRECTIONS.length; d++) {
-            if (solve(matrix, row + DIRECTIONS[d][0], col + DIRECTIONS[d][1], n)) {
-                return true;
+    private static boolean solve(int[][] board, int k) {
+        int length = board.length;
+        for (int r = 0; r < length; r++) {
+            for (int c = 0; c < length; c++) {
+                if (placeQueen(board, r, c, k)) {
+                    return true;
+                }
             }
         }
-        matrix[row][col] = preValue;
-
         return false;
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                System.out.print(" " + board[row][col]);
+            }
+            System.out.println("");
+        }
+    }
+
+    private static boolean placeQueen(int[][] board, int row, int col, int k) {
+
+        int length = board.length;
+
+        if (k == 0) {
+            return true;
+        }
+
+        if (col >= length) {
+            row++;
+            col = 0;
+        }
+
+        if (isSafe(board, row, col)) {
+            board[row][col] = -1;
+            if (placeQueen(board, row, col + 1, k - 1)) {
+                return true;
+            } else {
+                board[row][col] = 0;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isSafe(int[][] board, int row, int col) {
+        if(row >= board.length || col >= board.length){
+            return false;
+        }
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == -1 || board[i][col] == -1) {
+                return false;
+            }
+        }
+
+        for (int r = row, c = col; r >= 0 && c >= 0; r--, c--) {
+            if (board[r][r] == -1) {
+                return false;
+            }
+        }
+
+
+        for (int r = row, c = col; r < board.length && c < board.length; r++, c++) {
+            if (board[r][r] == -1) {
+                return false;
+            }
+        }
+
+        for (int r = row, c = col; r < board.length && c >= 0; r++, c--) {
+            if (board[r][r] == -1) {
+                return false;
+            }
+        }
+
+        for (int r = row, c = col; r >= 0 && c < board.length; r--, c++) {
+            if (board[r][r] == -1) {
+                return false;
+            }
+        }
+        return true;
     }
 
 
