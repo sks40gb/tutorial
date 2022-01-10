61145ba29d5a05f712ca58c33708cec27ac3e46e
Sunil <sks.256@gmail.com>
2022-01-10 10:39:42 +0530

    Data Structure

--- datastructure/src/main/java/algorithm/Test.java
+++ datastructure/src/main/java/algorithm/Test.java
@@ -1,52 +1,34 @@
 package algorithm;
 
 public class Test {
-
     public static void main(String[] args) {
-        int[][] board = new int[4][4];
-        System.out.println(solve(board, 4));
-        printBoard(board);
+
+        int n = 4;
+        int[][] board = new int[n][n];
+        System.out.println(solved(board));
+        print(board);
     }
 
-    private static boolean solve(int[][] board, int k) {
-        int length = board.length;
-        for (int r = 0; r < length; r++) {
-            for (int c = 0; c < length; c++) {
-                if (placeQueen(board, r, c, k)) {
-                    return true;
-                }
-            }
+    private static boolean solved(int[][] board) {
+
+        int queens = board.length;
+        return queenPlaced(board, 0, queens);
+
+    }
+
+    private static boolean queenPlaced(int[][] board, int row, int queens) {
+        if (row >= board.length) {
+            return false;
         }
-        return false;
-    }
-
-    private static void printBoard(int[][] board) {
-        for (int row = 0; row < board.length; row++) {
-            for (int col = 0; col < board[0].length; col++) {
-                System.out.print(" " + board[row][col]);
-            }
-            System.out.println("");
-        }
-    }
-
-    private static boolean placeQueen(int[][] board, int row, int col, int k) {
-
-        int length = board.length;
-
-        if (k == 0) {
+        if (queens == 0) {
             return true;
         }
-
-        if (col >= length) {
-            row++;
-            col = 0;
-        }
-
-        if (isSafe(board, row, col)) {
-            board[row][col] = -1;
-            if (placeQueen(board, row, col + 1, k - 1)) {
-                return true;
-            } else {
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 1;
+                if (queenPlaced(board, row + 1, queens - 1)) {
+                    return true;
+                }
                 board[row][col] = 0;
             }
         }
@@ -54,41 +36,34 @@ public class Test {
     }
 
     private static boolean isSafe(int[][] board, int row, int col) {
-        if(row >= board.length || col >= board.length){
-            return false;
-        }
+
         for (int i = 0; i < board.length; i++) {
-            if (board[row][i] == -1 || board[i][col] == -1) {
+            if (board[row][i] == 1 || board[i][col] == 1) {
                 return false;
             }
         }
 
-        for (int r = row, c = col; r >= 0 && c >= 0; r--, c--) {
-            if (board[r][r] == -1) {
+        for (int i = 0, j = 0; i < row && j < col; i++, j++) {
+            if (board[i][j] == 1) {
                 return false;
             }
         }
 
-
-        for (int r = row, c = col; r < board.length && c < board.length; r++, c++) {
-            if (board[r][r] == -1) {
-                return false;
-            }
-        }
-
-        for (int r = row, c = col; r < board.length && c >= 0; r++, c--) {
-            if (board[r][r] == -1) {
-                return false;
-            }
-        }
-
-        for (int r = row, c = col; r >= 0 && c < board.length; r--, c++) {
-            if (board[r][r] == -1) {
+        for (int i = row, j = col; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 1) {
                 return false;
             }
         }
         return true;
     }
 
+    private static void print(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
 }
