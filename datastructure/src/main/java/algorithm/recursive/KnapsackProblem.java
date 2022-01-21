7adf2fbc34d34fe83159fd050066ec43e17af617
Sunil <sks.256@gmail.com>
2022-01-21 21:05:03 +0530

    Data Structure

--- datastructure/src/main/java/algorithm/recursive/KnapsackProblem.java
+++ datastructure/src/main/java/algorithm/recursive/KnapsackProblem.java
@@ -25,7 +25,8 @@ public class KnapsackProblem {
         int[] wt = new int[]{10, 20, 30};
         int W = 40;
         System.out.println(knapSack(val, wt, W, 0, 0));
-        System.out.println(knapSack(W, val, wt, val.length));
+        System.out.println(knapSack(W, wt, val, val.length));
+        System.out.println(knapSackDP(W, wt, val, val.length));
 
     }
 
@@ -69,7 +70,7 @@ public class KnapsackProblem {
     /**
      * Iterative version of above format
      */
-    int knapSackDP(int W, int wt[], int val[], int n) {
+    static int knapSackDP(int W, int wt[], int val[], int n) {
         int i, w;
         int K[][] = new int[n + 1][W + 1];
 
@@ -77,13 +78,13 @@ public class KnapsackProblem {
             for (w = 0; w <= W; w++) {
                 if (i == 0 || w == 0) {
                     K[i][w] = 0;
-                } else if (wt[i - 1] <= w) {
+                } else if (wt[i - 1] > w) {
+                    K[i][w] = K[i - 1][w];
+                } else {
                     K[i][w] = Math.max(
                         val[i - 1] + K[i - 1][w - wt[i - 1]],
                         K[i - 1][w]
                     );
-                } else {
-                    K[i][w] = K[i - 1][w];
                 }
             }
         }
