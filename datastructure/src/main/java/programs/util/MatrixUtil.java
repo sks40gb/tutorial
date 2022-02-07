5c7f2634d363840005fb75c7a49cd7ae1ae6e6bd
Sunil <sks.256@gmail.com>
2022-02-07 11:58:18 +0530

    Data Structure

---
+++ datastructure/src/main/java/programs/util/MatrixUtil.java
@@ -0,0 +1,14 @@
+package programs.util;
+
+public class MatrixUtil {
+
+    public static void printMatrix(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println("");
+        }
+
+    }
+}
